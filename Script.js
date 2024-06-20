c8c6212949f4b60afa8febc3f75f30efd66ef5ea
Tiago José <105284869+ti4goJOSE@users.noreply.github.com>
2024-06-20 10:16:46 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const cixaAlternativa = documnet.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
+const perguntas = [
+    { //primeiro objeto da lista de perguntas
+        enunciado: "pergunta1",
+    alternativas: ["alternativa1", "alternativa2"]
+},
+{// segundo pbjeto da lista de perguntas
+    enunciado: "pergunta2",
+alternativas: ["alternativa1", "alternativa2"]
+}
+];
